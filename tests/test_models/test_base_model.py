053a6c3ec555f3399e654b2012605e0c0a602a52
yandah1 <nonku003@gmail.com>
2023-12-20 16:15:50 +0200

    task7

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,46 +1,56 @@
 #!/usr/bin/python3
 """ """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import unittest
 import datetime
 from uuid import UUID
 import json
 import os
 
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        'basemodel test not supported')
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """base mode class test"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """init basemodel """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """set up method of the test class """
         pass
 
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
+    def test_init(self):
+        """Tests the initialization"""
+        self.assertIsInstance(self.value(), BaseModel)
+        if self.value is not BaseModel:
+            self.assertIsInstance(self.value(), Base)
+        else:
+            self.assertNotIsInstance(self.value(), Base)
+
     def test_default(self):
-        """ """
+        """ default testing of basemodel """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """kwargs testing of basemodel"""
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """ kwargs_int testing of basemodel """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -57,19 +67,61 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """Testing str methond """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
+            i.__dict__))
 
     def test_todict(self):
-        """ """
+        """ testing the to_dict method """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
+        self.assertIsInstance(self.value().to_dict(), dict)
+        self.assertIn('id', self.value().to_dict())
+        self.assertIn('created_at', self.value().to_dict())
+        self.assertIn('updated_at', self.value().to_dict())
+        mdl = self.value()
+        mdl.firstname = 'Celestine'
+        mdl.lastname = 'Akpanoko'
+        self.assertIn('firstname', mdl.to_dict())
+        self.assertIn('lastname', mdl.to_dict())
+        self.assertIn('firstname', self.value(firstname='Celestine').to_dict())
+        self.assertIn('lastname', self.value(lastname='Akpanoko').to_dict())
+        # Tests to_dict datetime attributes if they are strings
+        self.assertIsInstance(self.value().to_dict()['created_at'], str)
+        self.assertIsInstance(self.value().to_dict()['updated_at'], str)
+        # Tests to_dict output
+        datetime_now = datetime.today()
+        mdl = self.value()
+        mdl.id = '012345'
+        mdl.created_at = mdl.updated_at = datetime_now
+        to_dict = {
+                'id': '012345',
+                '__class__': mdl.__class__.__name__,
+                'created_at': datetime_now.isoformat(),
+                'updated_at': datetime_now.isoformat()
+                }
+        self.assertDictEqual(mdl.to_dict(), to_dict)
+        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+            self.assertDictEqual(
+                    self.value(id='u-b34', age=13).to_dict(),
+                    {
+                        '__class__': mdl.__class__.__name__,
+                        'id': 'u-b34',
+                        'age': 13
+                        }
+                    )
+            with self.assertRaises(TypeError):
+                self.value().to_dict(None)
+            with self.assertRaises(TypeError):
+                self.value().to_dict(self.value())
+            with self.assertRaises(TypeError):
+                self.value().to_dict(45)
+            self.assertNotIn('_sa_instance_state', n)
 
     def test_kwargs_none(self):
-        """ """
+        """testing kwargs again with non """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
@@ -77,8 +129,8 @@ class test_basemodel(unittest.TestCase):
     def test_kwargs_one(self):
         """ """
         n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        new = self.value(**n)
+        self.assertEqual(new.name, n['Name'])
 
     def test_id(self):
         """ """
