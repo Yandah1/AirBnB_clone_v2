ae9a1c81296a2440d780245ddb04587980221336
yandah1 <nonku003@gmail.com>
2024-01-28 20:01:02 +0200

    HBNB filters

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,9 +15,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a list of all State objects sorted by name."""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-
+    states = storage.all(State)
     return render_template('7-states_list.html', states=sorted_states)
 
 
